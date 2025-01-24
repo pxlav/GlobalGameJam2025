3f720f981a32e780bb6b3b6389effe44f4a3be4a
Michał K <55460422+pxlav@users.noreply.github.com>
2025-01-24 20:49:02 +0100

    Ferst iszu

--- Assets/Scripts/Bubble.cs
+++ Assets/Scripts/Bubble.cs
@@ -1,18 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using Unity.PlasticSCM.Editor.WebApi;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Bubble : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float speed;
+    public int currentTarget;
+    public List<Transform> targets;
+    public int targetsNumber;
+    public Transform[] targetsToAdd;
+    private void Start()
     {
-        
+        speed = 0.5f;
+        targetsNumber = 4;
+        targets.AddRange(GameObject.FindGameObjectsWithTag("Target"));
     }
-
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        this.transform.position = Vector3.MoveTowards(transform.position, targets.ElementAt(currentTarget).transform.position, speed);
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Target")
+        {
+            currentTarget++;
+        }
     }
 }
